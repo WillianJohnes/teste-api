5505cbb80701b7c9fe978a03b9535fa62b246265
Willian Johnes <willianjohnes@github.com>
2025-06-02 11:29:22 -0300

    Corrige localização dos step definitions

--- cypress/e2e/step_definitions/delete_device.cy.js
+++ cypress/e2e/step_definitions/delete_device.cy.js
@@ -3,7 +3,7 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 let deviceId;
 
 Given("que eu tenha um dispositivo criado na API", () => {
-  const body = {
+  cy.request("POST", "https://api.restful-api.dev/objects", {
     name: "Samsung Galaxy S22 ULTRA",
     data: {
       year: 2022,
@@ -11,17 +11,15 @@ Given("que eu tenha um dispositivo criado na API", () => {
       "CPU model": "Snapdragon Gen8 Pro",
       "Hard disk size": "512 GB"
     }
-  };
-
-  cy.request("POST", "https://api.restful-api.dev/objects", body).then((response) => {
+  }).then((response) => {
     expect(response.status).to.eq(200);
     deviceId = response.body.id;
-    expect(deviceId).to.exist;
   });
 });
 
 When("eu deletar esse dispositivo", () => {
-  cy.request("DELETE", `https://api.restful-api.dev/objects/${deviceId}`).as("deleteDeviceResult");
+  cy.request("DELETE", `https://api.restful-api.dev/objects/${deviceId}`)
+    .as("deleteDeviceResult");
 });
 
 Then("o dispositivo deve ser removido com sucesso", () => {
