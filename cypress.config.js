9dc80dfd70fa84caacb901cbb93431ca56382024
Willian Johnes <willianjohnes@github.com>
2025-06-02 11:12:24 -0300

    Corrige config para usar esbuild e Cucumber moderno e adiciona o gitgnore

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,20 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const browserify = require("@cypress/browserify-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      addCucumberPreprocessorPlugin(on, config);
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
       on("file:preprocessor", createBundler({
         plugins: [createEsbuildPlugin.default(config)],
       }));
       return config;
     },
-    specPattern: "cypress/e2e/**/*.feature",
     baseUrl: "https://api.restful-api.dev",
+    specPattern: "cypress/e2e/**/*.feature",
+    supportFile: false,
     reporter: "mochawesome",
     reporterOptions: {
       reportDir: "cypress/reports/mochawesome",
